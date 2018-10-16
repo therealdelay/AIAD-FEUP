98697fd9340aef0ff2d2a60c783ec5c9f14e434c
therealdelay <danny.a_soares@hotmail.com>
2018-10-16 17:45:22 +0100

    meh

--- Project/src/agents/Student.java
+++ Project/src/agents/Student.java
@@ -1,13 +1,35 @@
 package agents;
 
+import jade.core.AID;
 import jade.core.Agent;
+import jade.core.behaviours.Behaviour;
 import jade.core.behaviours.CyclicBehaviour;
 import jade.lang.acl.ACLMessage;
 
 public class Student extends Agent {
 	
+	boolean didEat = false;
 	
 	protected void setup() {
+		addBehaviour(new EatingBehaviour());
+		addBehaviour(new ListeningBehaviour());
+		
+	}
+	
+	class EatingBehaviour extends Behaviour {
+
+		@Override
+		public void action() {
+			ACLMessage msg = new ACLMessage(ACLMessage.INFORM);
+			msg.setContent("1 lunch.");
+			msg.addReceiver(new AID("canteen1", AID.ISLOCALNAME));
+			send(msg);
+		}
+
+		@Override
+		public boolean done() {
+			return didEat;
+		}
 		
 	}
 	
@@ -16,16 +38,16 @@ public class Student extends Agent {
 		public void action() {
 			ACLMessage msg = receive();
 			if(msg != null) {
-				System.out.println(msg);
-				ACLMessage reply = msg.createReply();
-				reply.setPerformative(ACLMessage.INFORM);
-				reply.setContent("Got your message!");
-				send(reply);
+				String msgCnt = msg.getContent();
+				System.out.println("Resposta da cantina: " + msgCnt);
+				didEat = true;
 			} else {
 				block();
 			}
 		}
 
 	}
+	
+	
 	
 }
