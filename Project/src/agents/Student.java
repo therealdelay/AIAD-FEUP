2d222e025a3014d2b13ec8232c567c7b2a55284f
beemargarida <up201505505@gcloud.fe.up.pt>
2018-10-23 18:42:29 +0100

    Simple call for proposal implemented to chose a canteen between students

--- Project/src/agents/Student.java
+++ Project/src/agents/Student.java
@@ -1,6 +1,8 @@
 package agents;
 
 import java.util.Random;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import jade.core.AID;
 import jade.core.Agent;
@@ -17,14 +19,21 @@ public class Student extends Agent {
 	
 	int didEat = 0;
 	DFAgentDescription[] canteens = null;
+	DFAgentDescription[] students = null;
 	int canteenOption = -1;
+	
+	boolean sent_proposal = false;
+	boolean canteen_chosen = false;
+	int votes = 0;
 	
 	protected void setup() {
 		registerOnDF();
 		searchAllCanteens();
+		searchAllStudents();
 		canteenOption = chooseCanteen();
-		addBehaviour(new EatingBehaviour());
-		addBehaviour(new ListeningBehaviour());
+		//addBehaviour(new EatingBehaviour());
+		//addBehaviour(new ListeningBehaviour());
+		addBehaviour(new ProposeCanteenBehaviour());
 		
 	}
 	
@@ -43,6 +52,19 @@ public class Student extends Agent {
 		} catch (FIPAException fe) {
 			fe.printStackTrace();
 		} 
+	}
+	
+	public void searchAllStudents() {
+		DFAgentDescription template = new DFAgentDescription();
+		ServiceDescription sd = new ServiceDescription();
+		sd.setType("student");
+		template.addServices(sd);
+		try {
+			students = DFService.search(this, template);
+			
+		} catch (FIPAException fe) {
+			fe.printStackTrace();
+		}
 	}
 	
 	protected void registerOnDF() {
@@ -90,6 +112,79 @@ public class Student extends Agent {
 			}
 		}
 
+	}
+	
+	class ProposeCanteenBehaviour extends Behaviour {
+		
+		Timer timer;
+		
+		public void action() {
+			Random timeout = new Random();
+			int timeoutTime = timeout.nextInt(10000);
+			timer = new Timer();
+			timer.schedule(new TimerTask() {
+
+				public void run() {
+					if(!sent_proposal && !canteen_chosen) {
+						
+						searchAllStudents();
+						ACLMessage msg = new ACLMessage(ACLMessage.PROPOSE);
+						msg.setContent("FEUP");
+						
+						for(int i = 0; i < students.length; i++) {
+							if(!students[i].getName().getLocalName().equals(getLocalName())) {
+								msg.addReceiver(new AID(students[i].getName().getLocalName(), AID.ISLOCALNAME));
+							}
+						}			
+						
+						send(msg);
+						sent_proposal = true;						
+					}
+				  }
+				}, timeoutTime);
+			
+			
+			
+			ACLMessage msg = receive();
+			if(msg != null) {
+				
+				if(msg.getPerformative() == ACLMessage.PROPOSE) {
+					
+					timer.cancel();
+					timer.purge();
+					ACLMessage reply = msg.createReply();
+					reply.setPerformative(ACLMessage.ACCEPT_PROPOSAL);
+					send(reply);
+					canteen_chosen = true;
+				}
+				else if (msg.getPerformative() == ACLMessage.ACCEPT_PROPOSAL && sent_proposal) {
+					votes++;
+				}
+				
+			} else {
+				block();
+			}
+			
+			
+		}
+
+		@Override
+		public boolean done() {
+			if(students != null)
+				
+				if(votes > students.length/2) {
+					
+					votes = 0;
+					canteen_chosen = false;
+					sent_proposal = false;
+					return true;
+					
+				}
+				else 
+					return false;
+			return canteen_chosen;
+		}
+		
 	}
 	
 	
